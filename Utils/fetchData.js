b26078d8b94305696cca5d23c0a2a30abf83ebf8
ahmadysalah <a.ahmad.salah@gmail.com>
2020-09-16 20:59:45 +0300

    Add Local host as defult Proxy [Developer-Mod] create api request function to prepare proxy server as localhost create the fetch data javascript to use it insted of import axios every time we need to send reqest to the server good luck #9'

---
+++ Utils/fetchData.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import api from './apiRequest';
+
+const fetchData = async (url, body, method) => {
+  switch (method) {
+    case undefined:
+      return axios
+        .get(api.concat(url))
+        .then(({ data }) => data)
+        .catch(() => 'Error get data');
+
+    default:
+      axios[method](api.concat(url), body)
+        .then(({ data }) => data)
+        .catch(() => `Error ${method} data`);
+      break;
+  }
+};
+
+export default fetchData;
