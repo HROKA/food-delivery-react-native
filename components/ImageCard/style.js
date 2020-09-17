685e0a0fb40ac9b6fc83b453a9da23b381dee7f0
ahmadysalah <a.ahmad.salah@gmail.com>
2020-09-17 16:03:38 +0300

    Image-card main component - create the image component - create main style - change some rules in eslint - vertecal component in progress #12

---
+++ components/ImageCard/style.js
@@ -0,0 +1,32 @@
+import { StyleSheet } from 'react-native';
+import mainStyle from '../../style';
+
+const styles = StyleSheet.create({
+  element: {
+    width: mainStyle.deviceHight / 6,
+    height: mainStyle.deviceHight / 6,
+  },
+  imageBackground: {
+    borderRadius: 12,
+    width: '100%',
+    height: '100%',
+  },
+  title: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    borderRadius: 12,
+    height: '100%',
+    width: '100%',
+    backgroundColor:
+      'linear-gradient(   rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.7))',
+    position: 'absolute',
+    bottom: 0,
+  },
+  titleText: {
+    color: 'white',
+    margin: 5,
+  },
+});
+
+export default styles;
---
+++ style.js
@@ -0,0 +1,10 @@
+import { Dimensions } from 'react-native';
+
+const mainStyle = {
+  primaryColor: '',
+  secondaryColor: '',
+  deviceWidth: Dimensions.get('screen').width,
+  deviceHight: Dimensions.get('screen').height,
+};
+
+export default mainStyle;
