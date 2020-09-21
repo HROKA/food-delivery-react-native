ca8eda8a29950474755dd37ed7a7ef1b69a553c6
hassanannajjar <hassan.an.najjar@gmail.com>
2020-09-21 16:08:03 +0300

    added all screens

---
+++ screens/Favorites/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from './style';
+
+const Favorites = () => (
+    <View style={styles.container}>
+        <Text>Favorites</Text>
+    </View>
+);
+
+export default Favorites;
